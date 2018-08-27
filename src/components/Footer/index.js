fe49b805f43f50dbf862e4a7ee6a4b903b289ec8
Andrei Zvonkov <andrei@appnovation.com>
2018-08-26 22:16:56 -0400

    footer updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,6 +10,7 @@ function Footer() {
     <Container>
       <a href="https://github.com/zetagraph"><Img src={require(`./Github.svg`)} alt="Icon" /></a>
       <a href="https://twitter.com/zetagraph"><Img src={require(`./Twitter.svg`)} alt="Icon" /></a>
+      <a href="https://medium.com/@zetagraph"><Img src={require(`./Medium.svg`)} alt="Icon" /></a>
       <a href="https://www.linkedin.com/in/andreizvonkov"><Img src={require(`./Linkedin.svg`)} alt="Icon" /></a>
       <a href="mailto:zvonkov@gmail.com"><Img src={require(`./Email.svg`)} alt="Icon" /></a>
     </Container>
